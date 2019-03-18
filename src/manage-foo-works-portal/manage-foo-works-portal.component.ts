cd7dedbdc5ebc5d47380af8d7286a22a0d176711
unknown <Administrator@JHKIM-DOCKER.wingroup.windeploy.ntdev.microsoft.com>
2019-03-18 14:34:33 -0700

    first commit

---
+++ src/manage-foo-works-portal/manage-foo-works-portal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-foo-works-portal',
+  templateUrl: './manage-foo-works-portal.component.html',
+  styleUrls: ['./manage-foo-works-portal.component.css']
+})
+export class ManageFooWorksPortalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/manage-foo-works-portal/manage-foo-works-portal.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ManageFooWorksPortalComponent } from './manage-foo-works-portal.component';
+
+@NgModule({
+  declarations: [ManageFooWorksPortalComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ManageFooWorksPortalModule { }
